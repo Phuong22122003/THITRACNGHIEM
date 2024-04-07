8c07a88650e5d297f8e64bff48851f5f98be896b
Phuong22122003 <hnguyenphuong09@gmail.com>
2024-04-07 22:49:54 +0700

    add ds thi

---
+++ THITRACNGHIEM/DS_THI.cs
@@ -0,0 +1,18 @@
+﻿namespace THITRACNGHIEM
+{
+
+
+    partial class DS_THI
+    {
+        partial class SP_LAYLICHTHIDataTable
+        {
+        }
+    }
+}
+
+namespace THITRACNGHIEM.DS_THITableAdapters {
+    
+    
+    public partial class SP_LAYCAUHOITHITableAdapter {
+    }
+}
